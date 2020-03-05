66cd55bccfb1d8f36652fceda3b2abbfbc7b7d01
Alberto <avicente@europe.altair.com>
2020-03-05 13:29:59 +0100

    updated unity-text-input-react

--- src/components/unity-text-input-react/UnityTextInput.tsx
+++ src/components/unity-text-input-react/UnityTextInput.tsx
@@ -1,38 +1,6 @@
 import React, { Component } from 'react'
 import '@bit/smartworks.unity.unity-text-input'
-
-interface TextInputProps {
-  value?: string,
-  onChange?: Function,
-  label?: string,
-  remark?: string,
-  units?: string,
-  placeholder?: string,
-  error?: string,
-  validation?: Function,
-  disabled?: boolean,
-  time?: boolean,
-  maxlength?: number,
-  hint?: string,
-  password?: boolean,
-  charCount?: boolean,
-  showIcon?: boolean,
-  onKeyDown?:string,
-  onBlur?:string,
-  onDragStart?:string,
-  onDrop?:string,
-  onFocus?:string,
-  hideBorder?:boolean,
-  rounded?:boolean,
-  innerLeftIcon?:string,
-  innerRightIcon?:string,
-  style?:string,
-  borderEffects?: boolean,
-  area?: boolean,
-  minLines?: number,
-  maxLines?: number
-}
-
+import { TextInputProps } from '@bit/smartworks.unity.unity-text-input'
 
 export default class UnityTextInput extends Component<TextInputProps> {
 
@@ -53,52 +21,7 @@ export default class UnityTextInput extends Component<TextInputProps> {
   }
 
   render() {
-    const {
-      value='',
-      label='',
-      remark='',
-      units='',
-      placeholder='',
-      error='',
-      disabled=false,
-      time=false,
-      password=false,
-      charCount=false,
-      showIcon=false,
-      onKeyDown,
-      onBlur,
-      onDragStart,
-      onDrop,
-      onFocus,
-      hideBorder,
-      rounded,
-      innerLeftIcon,
-      innerRightIcon,
-      style
-    } = this.props
-    let inputProps : TextInputProps = {
-      value,
-      label,
-      units,
-      placeholder,
-      innerLeftIcon,
-      innerRightIcon
-    }
-
-    if (!!error) inputProps.error = error
-    if (!!remark) inputProps.remark = remark
-    if (!!disabled) inputProps.disabled = disabled
-    if (!!time) inputProps.time = time
-    if (!!password) inputProps.password = password
-    if (!!charCount) inputProps.charCount = charCount
-    if (!!showIcon) inputProps.showIcon = showIcon
-    if (!!onKeyDown) inputProps.onKeyDown = onKeyDown
-    if (!!onBlur) inputProps.onBlur = onBlur
-    if (!!onDragStart) inputProps.onDragStart = onDragStart
-    if (!!onDrop) inputProps.onDrop = onDrop
-    if (!!onFocus) inputProps.onFocus = onFocus
-    if (!!hideBorder) inputProps.hideBorder = hideBorder
-    if (!!rounded) inputProps.rounded = rounded
+    const {style, ...inputProps} = this.props
 
     return <unity-text-input
       style={style}
