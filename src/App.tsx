4d359136366dc729a99b33320317dcebd8524950
Andrew Heath <aheath@altair.com>
2020-03-06 11:11:05 -0800

    fix merge issues

--- src/App.tsx
+++ src/App.tsx
@@ -46,6 +46,7 @@ class App extends React.Component {
               value={this.state.value}
               minLines={7}
               maxLines={19}
+              showError
             />
           </div>
         </UnitySection>
