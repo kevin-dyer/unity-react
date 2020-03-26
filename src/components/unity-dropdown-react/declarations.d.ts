f8c964387a3d87619befa62428d13eb46940c50b
Andrew Heath <aheath@altair.com>
2020-03-26 00:35:18 -0700

    add dropdown declarations

---
+++ src/components/unity-dropdown-react/declarations.d.ts
@@ -0,0 +1,27 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "unity-dropdown": any;
+  }
+}
+
+declare module '@bit/smartworks.unity.unity-dropdown' {
+  export type inputType = "menu" | "single-select" | "multi-select"
+  export type boxType = "label" | "search" | "button-gradient" | "button-outlined" | "inline"
+  import CSSProperties from 'react'
+  export interface dropdownPropsType {
+    [key: string]: any;
+    label?: string,
+    inputType?: inputType,
+    boxType?: boxType,
+    placeholder?: string,
+    options?: Object[] | string,
+    selected?: string[],
+    disabled?: boolean,
+    onMenuClick?: Function,
+    selectIcon?: boolean,
+    helperText?: string,
+    searchBox?: boolean,
+    showTags?: boolean,
+    onValueChange?: Function
+  }
+}
