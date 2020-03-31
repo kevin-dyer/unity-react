83e18f375513b756440ee246406f4e1caa1e857d
Andrew Heath <aheath@altair.com>
2020-03-31 15:58:03 -0700

    add ref and function updaters to dropdown

--- src/components/unity-dropdown-react/UnityDropdown.tsx
+++ src/components/unity-dropdown-react/UnityDropdown.tsx
@@ -4,23 +4,48 @@ import '@bit/smartworks.unity.unity-dropdown'
 import { dropdownPropsType } from '@bit/smartworks.unity.unity-dropdown'
 
 export default class UnityDropdown extends Component<dropdownPropsType> {
-  render() {
+  private dropdownRef = React.createRef<dropdownPropsType>()
+
+  componentDidMount = () => {
+    this.updateProperties()
+  }
+
+  componentDidUpdate(oldProps: dropdownPropsType) {
+    this.updateProperties(oldProps)
+  }
+
+  updateProperties(oldProps: dropdownPropsType={}) {
     const {
       onMenuClick,
-      onValueChange,
+      onValueChange
+    } = this.props
+
+    const {
+      onMenuClick: oldOnMenuClick,
+      onValueChange: oldOnValueChange
+    } = oldProps
+
+    const unityDropdown = this.dropdownRef.current
+    if (unityDropdown) {
+      if (oldOnMenuClick !== onMenuClick) unityDropdown.onMenuClick = onMenuClick
+      if (oldOnValueChange !== onValueChange) unityDropdown.onValueChange = onValueChange
+    }
+  }
+
+  render() {
+    const {
       options=[],
       selected=[],
       ...otherProps
     } = this.props
     const dropdownProps : dropdownPropsType = {
-      onMenuClick: !otherProps.disabled ? onMenuClick : ()=>{},
-      onValueChange: !otherProps.disabled ? onValueChange : ()=>{},
       options: JSON.stringify(options),
       selected: JSON.stringify(selected),
       ...otherProps
     }
     return (
       <unity-dropdown
+        ref={this.dropdownRef}
         {...dropdownProps}
       ></unity-dropdown>
     )
