a3392050f29c6275effb8197aea1df7b86f636c7
Alberto <avicente@europe.altair.com>
2020-04-14 18:11:57 +0200

    wrapper for unity-modal

---
+++ src/components/unity-modal-react/declarations.d.ts
@@ -0,0 +1,7 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "unity-modal": any;
+  }
+}
+
+declare module '@bit/smartworks.unity.unity-modal'
---
+++ src/components/unity-modal-react/unity-modal-react.md
@@ -0,0 +1,6 @@
+# unity-modal-react
+
+## Changelog:
+
+### v0.1.0
+- intial version based on unity-modal v0.1.7
