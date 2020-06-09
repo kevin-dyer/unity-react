008ac8f3ecf5d85c8e6e8c052a9294b90167cd67
Andrew Heath <aheath@altair.com>
2020-06-09 00:55:40 -0700

    fix leftover partial merge

--- src/App.tsx
+++ src/App.tsx
@@ -617,7 +617,6 @@ class App extends React.Component {
                   </div>
                 </UnitySection>
               </UnitySection>
-<<<<<<< HEAD
               <UnitySection style={{height: "1000px", overflow: 'scroll', "--vert-pos": "top"}}>
                 <div style={{...contentBox, display: "flex", flexDirection: "column"}}>
                     <UnitySection style={{flex: 0}}>
@@ -660,7 +659,7 @@ class App extends React.Component {
                           }}
                           showTags
                         />
-                        {!!this.tableRef && !!this.tableRef.current && 
+                        {!!this.tableRef && !!this.tableRef.current &&
                           <UnityTableExport
                             style={{margin: "4px", alignSelf: "flex-end", marginLeft: 40}}
                             tableRef={this.tableRef.current.tableRef}
@@ -673,7 +672,7 @@ class App extends React.Component {
                             <UnityButton
                               type="solid"
                               label="Export"
-                              rightIcon="unity:file_download"  
+                              rightIcon="unity:file_download"
                               onClick={() => console.log(`UnityButton child on UnityTableExport received click event`)}
                             />
                           </UnityTableExport>
@@ -718,113 +717,8 @@ class App extends React.Component {
                       })}
                     />
                   </div>
-                </UnityNotificationsHandler> 
+                </UnityNotificationsHandler>
               </div>
-=======
-            </UnitySection>
-            <UnitySection style={{height: "500px", "--vert-pos": "top"}}>
-              <div style={{...contentBox, display: "flex", flexDirection: "column"}}>
-                  <UnitySection style={{flex: 0}}>
-                    <h3>UnityTable</h3>
-                    <UnitySection style={{"--horz-pos": "right"}}>
-                      <UnityDropdown
-                        label='Change Selection'
-                        inputType='multi-select'
-                        placeholder='Pick some'
-                        options={[
-                          {
-                            label: 'Africa',
-                            id: 'africa'
-                          },
-                          {
-                            label: 'Asia',
-                            id: 'asia'
-                          },
-                          {
-                            label: 'Australia',
-                            id: 'australia'
-                          },
-                          {
-                            label: 'Europe',
-                            id: 'europe'
-                          },
-                        ]}
-                        onValueChange={(selectedElements: string[], selected: boolean) => {
-                          const newSelection: string[] = [...this.state.selection]
-                          selectedElements.forEach((element: string) => {
-                            if (selected) {
-                              newSelection.push(element)
-                              return
-                            }
-                            if (newSelection.includes(element)) {
-                              newSelection.splice(newSelection.indexOf(element), 1)
-                            }
-                          })
-                          this.setState({ selection: newSelection })
-                        }}
-                        showTags
-                      />
-                      {!!this.tableRef && !!this.tableRef.current &&
-                        <UnityTableExport
-                          style={{margin: "4px", alignSelf: "flex-end", marginLeft: 40}}
-                          tableRef={this.tableRef.current.tableRef}
-                          beforeExport={(data: object) => {
-                            console.log(`processing data: `, data);
-                            return data
-                          }}
-                          onExport={() => console.log(`Exported table data`)}
-                        >
-                          <UnityButton
-                            type="solid"
-                            label="Export"
-                            rightIcon="unity:file_download"
-                            onClick={() => console.log(`UnityButton child on UnityTableExport received click event`)}
-                          />
-                        </UnityTableExport>
-                      }
-                    </UnitySection>
-                  </UnitySection>
-                {console.log('columns', columns)}
-                <UnityTable
-                  ref={this.tableRef}
-                  data={data}
-                  columns={columns}
-                  childKeys={childKeys}
-                  selected={selection}
-                  keyExtractor={(node: any) => node.id}
-                  selectable
-                  onSelectionChange={(selection: []) => console.log(`new selection:`, selection)}
-                />
-              </div>
-            </UnitySection>
-            <div
-              style={notificationSectionContainerStyle}
-            >
-              <WithNotificationsWrappedSection
-                text={'This text is being passed to the wrapped component as a prop.'}
-              />
-              <UnityNotificationsHandler
-                name='notifications-via-component'
-                allowDuplicates={true}
-                position={'top-left'}
-                style={notificationSectionStyle}
-              >
-                <div style={buttonContainerStyle}>
-                  <UnityButton
-                    type='solid'
-                    label='Add Notification'
-                    onClick={() => addNotification({
-                      name: 'notifications-via-component',
-                      notification: {
-                        type: 'help',
-                        text: 'What a helpful Notification'
-                      },
-                      timeout: 0
-                    })}
-                  />
-                </div>
-              </UnityNotificationsHandler>
->>>>>>> master
             </div>
           </div>
         </div>
